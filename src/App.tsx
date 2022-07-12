63dd67d6fc71778f0ae028c98dfe9ea7e19a5f29
Adrian Chojnacki <achojnacki@pgs-soft.com>
2022-07-12 15:37:23 +0200

    Add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Create from "./pages/Create";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
   );
