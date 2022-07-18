6a5c85ee3b0b97b03f3016a9b85ee31d9790b4e4
Adrian Chojnacki <achojnacki@pgs-soft.com>
2022-07-18 15:30:00 +0200

    Add proper grid

--- src/components/CreateForm/CreateForm.tsx
+++ src/components/CreateForm/CreateForm.tsx
@@ -3,41 +3,66 @@ import { Box, Grid, TextField } from "@mui/material";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import CreateFormCSS from "./CreateForm.module.css";
 
 const CreateForm = () => {
-  const [value, setValue] = React.useState<Date | null>(new Date());
+  const [creationDate, setCreationDate] = React.useState<Date | null>(
+    new Date(),
+  );
+  const [paymentDate, setPaymentDate] = React.useState<Date | null>(new Date());
+  const [amount, setAmount] = React.useState<number | null>(0);
 
-  const handleChange = (newValue: Date | null) => {
-    setValue(newValue);
+  const handleCreationDateChange = (newValue: Date | null) => {
+    setCreationDate(newValue);
+  };
+
+  const handlePaymentDateChange = (newValue: Date | null) => {
+    setPaymentDate(newValue);
+  };
+
+  const handleAmountChange = (event: any) => {
+    setAmount(event.target.value);
   };
 
   return (
     <form>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={1}>
-            <Grid item xs>
+          <Grid container spacing={2} rowSpacing={3}>
+            <Grid item xs={12} sm={4}>
               <DesktopDatePicker
                 label="Creation date"
                 inputFormat="MM/dd/yyyy"
-                value={value}
-                onChange={handleChange}
-                renderInput={(params: any) => <TextField {...params} />}
                 disablePast
+                value={creationDate}
+                onChange={handleCreationDateChange}
+                renderInput={(params: any) => (
+                  <TextField className={CreateFormCSS.input} {...params} />
+                )}
               />
             </Grid>
-            <Grid item xs>
+            <Grid item xs={12} sm={4}>
               <DesktopDatePicker
                 label="Payment date"
                 inputFormat="MM/dd/yyyy"
-                value={value}
-                onChange={handleChange}
-                renderInput={(params: any) => <TextField {...params} />}
                 disablePast
+                value={paymentDate}
+                onChange={handlePaymentDateChange}
+                renderInput={(params: any) => (
+                  <TextField className={CreateFormCSS.input} {...params} />
+                )}
               />
             </Grid>
-            <Grid item xs>
-              <TextField label="Amount" variant="outlined" />
+            <Grid item xs={12} sm={4}>
+              <TextField
+                label="Amount"
+                type="number"
+                variant="outlined"
+                InputProps={{ inputProps: { min: 0 } }}
+                value={amount}
+                onChange={handleAmountChange}
+                className={CreateFormCSS.input}
+              />
             </Grid>
           </Grid>
         </Box>
