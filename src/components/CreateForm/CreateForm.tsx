7f852cd6c44a4aedf5266ff981d379f6f2654a5b
Adrian Chojnacki <achojnacki@pgs-soft.com>
2022-07-19 11:37:39 +0200

    Add date formatter to onSubmit

--- src/components/CreateForm/CreateForm.tsx
+++ src/components/CreateForm/CreateForm.tsx
@@ -7,7 +7,7 @@ import CreateFormCSS from "./CreateForm.module.css";
 interface IFormInputs {
   creationDate: Date | null;
   paymentDate: Date | null;
-  input: number | null;
+  amount: string | null;
 }
 
 const CreateForm = () => {
@@ -17,7 +17,29 @@ const CreateForm = () => {
     // formState: { errors },
   } = useForm<IFormInputs>();
 
-  const onSubmit = (data: any) => console.log(data);
+  const onSubmit = (data: any) => {
+    let { creationDate, paymentDate, amount } = data;
+
+    let creationDay = creationDate.getDate();
+    if (creationDay < 10) creationDay = `0${creationDay}`;
+    let creationMonth = creationDate.getMonth() + 1;
+    if (creationMonth < 10) creationMonth = `0${creationMonth}`;
+    const creationYear = creationDate.getFullYear();
+
+    creationDate = `${creationDay}/${creationMonth}/${creationYear}`;
+
+    let paymentDay = paymentDate.getDate();
+    if (paymentDay < 10) paymentDay = `0${paymentDay}`;
+    let paymentMonth = paymentDate.getMonth() + 1;
+    if (paymentMonth < 10) paymentMonth = `0${paymentMonth}`;
+    const paymentYear = paymentDate.getFullYear();
+
+    paymentDate = `${paymentDay}/${paymentMonth}/${paymentYear}`;
+
+    amount = +amount;
+
+    console.log(creationDate, paymentDate, amount);
+  };
 
   return (
     <form action="/" method="POST" onSubmit={handleSubmit(onSubmit)}>
@@ -34,7 +56,7 @@ const CreateForm = () => {
                   <DesktopDatePicker
                     {...field}
                     label="Creation date"
-                    inputFormat="MM/dd/yyyy"
+                    inputFormat="dd/MM/yyyy"
                     disablePast
                     renderInput={(params) => (
                       <TextField {...params} className={CreateFormCSS.input} />
@@ -53,7 +75,7 @@ const CreateForm = () => {
                   <DesktopDatePicker
                     {...field}
                     label="Payment date"
-                    inputFormat="MM/dd/yyyy"
+                    inputFormat="dd/MM/yyyy"
                     disablePast
                     renderInput={(params) => (
                       <TextField {...params} className={CreateFormCSS.input} />
@@ -64,9 +86,9 @@ const CreateForm = () => {
             </Grid>
             <Grid item xs={12} sm={4}>
               <Controller
-                name="input"
+                name="amount"
                 control={control}
-                defaultValue={0}
+                defaultValue="0"
                 rules={{ required: true }}
                 render={({ field }) => (
                   <TextField
