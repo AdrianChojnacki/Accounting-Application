5a52e3f3632fc59979459ddc05a6c89500479ab3
Adrian Chojnacki <achojnacki@pgs-soft.com>
2022-07-27 11:02:34 +0200

    Add table to InvoiceDetails

--- src/components/InvoiceDetails/InvoiceDetails.tsx
+++ src/components/InvoiceDetails/InvoiceDetails.tsx
@@ -1,3 +1,12 @@
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+} from "@mui/material";
 import { IInvoiceDataProps } from "./InvoiceDetails.types";
 
 const InvoiceDetails = ({
@@ -6,12 +15,42 @@ const InvoiceDetails = ({
   invoiceData: IInvoiceDataProps;
 }) => {
   return (
-    <div>
-      <div>{invoiceData.id}</div>
-      <div>{invoiceData.created}</div>
-      <div>{invoiceData.until}</div>
-      <div>{invoiceData.amount}</div>
-    </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableBody>
+          <TableRow>
+            <TableCell colSpan={2}>
+              <Box fontWeight="bold" display="inline">
+                ID:
+              </Box>
+              {` ${invoiceData.id}`}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <Box fontWeight="bold" display="inline">
+                Created:
+              </Box>
+              {` ${invoiceData.created}`}
+            </TableCell>
+            <TableCell>
+              <Box fontWeight="bold" display="inline">
+                Payment until:
+              </Box>
+              {` ${invoiceData.until}`}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell colSpan={2} align="right">
+              <Box fontWeight="bold" display="inline">
+                Amount:
+              </Box>
+              {` ${invoiceData.amount}`}
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
