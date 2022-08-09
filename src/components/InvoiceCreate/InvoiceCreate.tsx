e58fa20ed2fae7f9cf27f1c099c578f007129e6b
Adrian Chojnacki <achojnacki@pgs-soft.com>
2022-08-09 17:55:59 +0200

    Add useLocalStorage custom-hook

--- src/components/InvoiceCreate/InvoiceCreate.tsx
+++ src/components/InvoiceCreate/InvoiceCreate.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, TextareaAutosize } from "@mui/material";
 import {
   DatePicker,
   ErrorMessage,
@@ -7,56 +7,68 @@ import {
   SubmitButton,
   SuccessPopup,
 } from "..";
+import { useLocalStorage } from "../../hooks";
 import { IInvoiceCreateProps } from ".";
 
 const InvoiceCreate = ({ errors, control }: IInvoiceCreateProps) => {
   const { t } = useTranslation();
+  const [note, setNote] = useLocalStorage(
+    "note",
+    t("noteKeptInTheLocalStorage"),
+  );
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2} rowSpacing={4}>
-        <Grid item xs={12} sm={4}>
-          <DatePicker
-            name="created"
-            control={control}
-            label={t("dateAdded")}
-            defaultDate={new Date()}
-            disabledPast
-          />
-          {errors.noCreationDate && <ErrorMessage text={t("pickADate")} />}
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <DatePicker
-            name="until"
-            control={control}
-            label={t("paymentDate")}
-            defaultDate={new Date()}
-            disabledPast
-          />
-          {errors.noPaymentDate && <ErrorMessage text={t("pickADate")} />}
-          {errors.paymentDateBeforeCreationDate && (
-            <ErrorMessage text={t("paymentDateCannotBeBeforeAddedDate")} />
-          )}
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <InputField
-            name="amount"
-            control={control}
-            label={t("amount")}
-            defaultValue={0}
-          />
-          {errors.noAmount && <ErrorMessage text={t("amountCannotBe0")} />}
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container justifyContent="flex-end">
-            <SubmitButton text={t("add")} />
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2} rowSpacing={4}>
+          <Grid item xs={12} sm={4}>
+            <DatePicker
+              name="created"
+              control={control}
+              label={t("dateAdded")}
+              defaultDate={new Date()}
+              disabledPast
+            />
+            {errors.noCreationDate && <ErrorMessage text={t("pickADate")} />}
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <DatePicker
+              name="until"
+              control={control}
+              label={t("paymentDate")}
+              defaultDate={new Date()}
+              disabledPast
+            />
+            {errors.noPaymentDate && <ErrorMessage text={t("pickADate")} />}
+            {errors.paymentDateBeforeCreationDate && (
+              <ErrorMessage text={t("paymentDateCannotBeBeforeAddedDate")} />
+            )}
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <InputField
+              name="amount"
+              control={control}
+              label={t("amount")}
+              defaultValue={0}
+            />
+            {errors.noAmount && <ErrorMessage text={t("amountCannotBe0")} />}
+          </Grid>
+          <Grid item xs={12}>
+            <Grid container justifyContent="flex-end">
+              <SubmitButton text={t("add")} />
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <TextareaAutosize
+              placeholder={t("addANote")}
+              value={note}
+              onChange={(e) => setNote(e.target.value)}
+            />
           </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <SuccessPopup text={t("invoiceAdded")} />
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+      <SuccessPopup text={t("invoiceAdded")} />
+    </>
   );
 };
 
--- src/components/InvoiceEdit/InvoiceEdit.tsx
+++ src/components/InvoiceEdit/InvoiceEdit.tsx
@@ -26,74 +26,74 @@ const InvoiceEdit = ({ errors, control, invoiceData }: IInvoiceEditProps) => {
 
   if (invoiceData) {
     invoice = (
-      <Grid container spacing={2} rowSpacing={4}>
-        <Grid item xs={12}>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableBody>
-                <TableRow>
-                  <TableCell colSpan={2}>
-                    <Box fontWeight="bold" display="inline">
-                      {t("id")}:
-                    </Box>
-                    {` ${invoiceData.id}`}
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <DatePicker
-                      name="created"
-                      control={control}
-                      label={t("dateAdded")}
-                      defaultDate={new Date(invoiceData.createdRaw)}
-                    />
-                    {errors.noCreationDate && (
-                      <ErrorMessage text={t("pickADate")} />
-                    )}
-                  </TableCell>
-                  <TableCell>
-                    <DatePicker
-                      name="until"
-                      control={control}
-                      label={t("paymentDate")}
-                      defaultDate={new Date(invoiceData.untilRaw)}
-                    />
-                    {errors.noPaymentDate && (
-                      <ErrorMessage text={t("pickADate")} />
-                    )}
-                    {errors.paymentDateBeforeCreationDate && (
-                      <ErrorMessage
-                        text={t("paymentDateCannotBeBeforeAddedDate")}
+      <>
+        <Grid container spacing={2} rowSpacing={4}>
+          <Grid item xs={12}>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableBody>
+                  <TableRow>
+                    <TableCell colSpan={2}>
+                      <Box fontWeight="bold" display="inline">
+                        {t("id")}:
+                      </Box>
+                      {` ${invoiceData.id}`}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <DatePicker
+                        name="created"
+                        control={control}
+                        label={t("dateAdded")}
+                        defaultDate={new Date(invoiceData.createdRaw)}
                       />
-                    )}
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell colSpan={2} align="right">
-                    <InputField
-                      name="amount"
-                      control={control}
-                      label={t("amount")}
-                      defaultValue={invoiceData.amount}
-                    />
-                    {errors.noAmount && (
-                      <ErrorMessage text={t("amountCannotBe0")} />
-                    )}
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container justifyContent="flex-end">
-            <SubmitButton text={t("save")} />
+                      {errors.noCreationDate && (
+                        <ErrorMessage text={t("pickADate")} />
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      <DatePicker
+                        name="until"
+                        control={control}
+                        label={t("paymentDate")}
+                        defaultDate={new Date(invoiceData.untilRaw)}
+                      />
+                      {errors.noPaymentDate && (
+                        <ErrorMessage text={t("pickADate")} />
+                      )}
+                      {errors.paymentDateBeforeCreationDate && (
+                        <ErrorMessage
+                          text={t("paymentDateCannotBeBeforeAddedDate")}
+                        />
+                      )}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell colSpan={2} align="right">
+                      <InputField
+                        name="amount"
+                        control={control}
+                        label={t("amount")}
+                        defaultValue={invoiceData.amount}
+                      />
+                      {errors.noAmount && (
+                        <ErrorMessage text={t("amountCannotBe0")} />
+                      )}
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid item xs={12}>
+            <Grid container justifyContent="flex-end">
+              <SubmitButton text={t("save")} />
+            </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <SuccessPopup text={t("changesSaved")} />
-        </Grid>
-      </Grid>
+        <SuccessPopup text={t("changesSaved")} />
+      </>
     );
   } else {
     invoice = <SkeletonEdit />;
