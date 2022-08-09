a78088ea21877b8ba98833ca4f22d8d852a1c902
Adrian Chojnacki <achojnacki@pgs-soft.com>
2022-08-09 18:25:10 +0200

    Add ref and onClick event

--- src/components/Signature/Signature.tsx
+++ src/components/Signature/Signature.tsx
@@ -1,7 +1,31 @@
+import { useRef } from "react";
 import SignatureCSS from "./Signature.module.css";
 
 const Signature = ({ text }: { text: string }) => {
-  return <p className={SignatureCSS.signature}>{text}</p>;
+  const signature = useRef<HTMLParagraphElement>(null);
+
+  const handleClick = () => {
+    if (
+      !signature.current!.style.color ||
+      signature.current!.style.color === "green"
+    ) {
+      signature.current!.style.color = "white";
+    } else if (signature.current!.style.color === "white") {
+      signature.current!.style.color = "yellow";
+    } else if (signature.current!.style.color === "yellow") {
+      signature.current!.style.color = "blue";
+    } else if (signature.current!.style.color === "blue") {
+      signature.current!.style.color = "red";
+    } else if (signature.current!.style.color === "red") {
+      signature.current!.style.color = "green";
+    }
+  };
+
+  return (
+    <p ref={signature} onClick={handleClick} className={SignatureCSS.signature}>
+      {text}
+    </p>
+  );
 };
 
 export { Signature };
