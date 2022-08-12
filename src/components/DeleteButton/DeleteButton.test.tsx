6b7a160f67baad4c2dca48bfdbeca97243aa07b4
Adrian Chojnacki <achojnacki@pgs-soft.com>
2022-08-12 15:52:21 +0200

    Add some more tests

--- src/components/DeleteButton/DeleteButton.test.tsx
+++ src/components/DeleteButton/DeleteButton.test.tsx
@@ -1,8 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { DeleteButton } from ".";
 
-test("should render DeleteButton component", () => {
-  render(<DeleteButton onClick={() => {}} />);
-  const deleteButton = screen.getByTestId("delete-button");
-  expect(deleteButton).toBeInTheDocument();
+describe("DeleteButton tests:", () => {
+  test("should render DeleteButton component", () => {
+    render(<DeleteButton onClick={() => {}} />);
+
+    const deleteButton = screen.getByTestId("delete-button");
+
+    expect(deleteButton).toBeInTheDocument();
+  });
+
+  test("DeleteButton should be clicked 1 time", () => {
+    const onClick = jest.fn();
+    render(<DeleteButton onClick={onClick} />);
+
+    const deleteButton = screen.getByTestId("delete-button");
+    fireEvent.click(deleteButton);
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
 });
